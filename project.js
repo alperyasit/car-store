cab1ba4cc8b9e347c2fe93ac40a87d8d7f6ee99d
alperyasit <alperyasit1@gmail.com>
2023-03-07 14:34:20 +0300

    Create project.js

---
+++ project.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("car-form");
+const titleElement = document.querySelector("#title");
+const priceElement = document.querySelector("#price");
+const urlElement = document.querySelector("#url");
+
+//  UI Objesi Açma
+
+const ui = new UI();
+
+//  Tüm Eventleri Yükleme
+
+eventListeners();
+
+function eventListeners() {
+  form.addEventListener("submit", addCar);
+}
+
+function addCar(e) {
+  const title = titleElement.value;
+  const price = priceElement.value;
+  const url = urlElement.value;
+
+  if (title === "" || price === "" || url === "") {
+    //hata
+  } else {
+    // Yeni Araç
+    const newCar = new Car(title, price, url);
+
+    ui.addCarToUI(newCar); // Arayüze Araç Ekleme
+  }
+}
