658865aca3f8f8472ece84e4ce306c308eeda16e
alperyasit <alperyasit1@gmail.com>
2023-03-08 13:43:53 +0300

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,5 +1,19 @@
 function Storage() {
-    Storage.prototype.addCarToStorage = function(newCar){
-     console.log(newCar);
-     } 
+  Storage.prototype.addCarToStorage = function (newCar) {
+    let cars = this.getCarsFromStorage();
+
+    cars.push(newCar);
+    localStorage.setItem("cars", JSON.stringify(cars));
+  };
+
+  Storage.prototype.getCarsFromStorage = function () {
+    let cars;
+
+    if (localStorage.getItem("cars") === null) {
+      cars = [];
+    } else {
+      cars = JSON.parse(localStorage.getItem("cars"));
+    }
+    return cars;
+  };
 }
