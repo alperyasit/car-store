3baf8f3cd61be5b03aedce5bcbbe239b6abe1bd6
alperyasit <alperyasit1@gmail.com>
2023-03-08 13:43:56 +0300

    Update ui.js

--- ui.js
+++ ui.js
@@ -40,3 +40,17 @@ setTimeout(function(){
 
 },2000);
 }
+
+UI.prototype.loadAllCars = function(cars){
+      const carList = document.getElementById("cars-card");
+      cars.forEach(function(car){
+        carList.innerHTML += `
+        <tr>
+           <td><img src="${car.url}" class="img-fluid img-thumbnail"></td>
+           <td>${car.title}</td>
+           <td>${car.price}</td>
+           <td><a href="#" id = "delete-car" class = "btn btn-danger">Aracı Sil</a></td>
+       </tr>
+       `
+      })
+}
